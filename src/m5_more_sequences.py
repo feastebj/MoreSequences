a3c2d30a783702b02ad9c6a82036cd4109b7b001
feastebj <feastebj@rose-hulman.edu>
2018-01-13 20:34:38 -0500

    m5 done

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -7,8 +7,8 @@ for ITERATING through SEQUENCES, including:
   -- The FIND pattern (via LINEAR SEARCH)
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Benjamin Feaster.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -76,7 +76,7 @@ def sum_radii(circles):
       :rtype: int | float
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -87,12 +87,18 @@ def sum_radii(circles):
     #
     #       Instead, use explicit loops, as you have for other problems.
     # ------------------------------------------------------------------
-
+    total = 0
+    for k in range(len(circles)):
+        circle_radius = circles[k].radius
+        total = total + circle_radius
+    return total
 
 # ----------------------------------------------------------------------
 # Some problems iterate (loop) through PART of the sequence,
 # perhaps BACKWARDS, as in the   count_last_n_odds   problem below.
 # ----------------------------------------------------------------------
+
+
 def run_test_count_last_n_odds():
     """ Tests the   count_last_n_odds   function. """
     print()
@@ -151,9 +157,18 @@ def count_last_n_odds(integers, n):
       :rtype: int
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    count = 0
+    integers = list(reversed(integers))
+    for k in range(integers[0], n):
+        if integers[k] % 2 == 1:
+            count = count + 1
+        else:
+            count = count + 0
+    return count
 
 
 # ----------------------------------------------------------------------
@@ -162,6 +177,8 @@ def count_last_n_odds(integers, n):
 # (or continuing to the end if it does NOT find the thing of interest),
 # as in the following problems:
 # ----------------------------------------------------------------------
+
+
 def run_test_index_of_first_negative():
     """ Tests the   index_of_first_negative   function. """
     print()
@@ -226,9 +243,13 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    for k in range(len(numbers)):
+        if numbers[k] % 2 == 1:
+            return numbers.index(numbers[k])
 
 
 def run_test_contains_an_a():
@@ -284,7 +305,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -297,6 +318,11 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # ------------------------------------------------------------------
+
+    for k in range(len(s)):
+        return 'a' in s
+    else:
+        return False
 
 
 # ----------------------------------------------------------------------
